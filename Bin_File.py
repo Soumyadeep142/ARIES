5a6b3ea96d8c0684fe2077d03c3fecb1ec3cfe7e
Soumyadeep142 <55705076+Soumyadeep142@users.noreply.github.com>
2021-06-23 23:10:08 +0530

    Add files via upload

---
+++ Bin_File.py
@@ -0,0 +1,19 @@
+from numpy import *
+from math import floor, ceil
+idn, mag, error, SD=loadtxt('Second_cut.txt', unpack='true')
+A=arange(floor(min(mag)), ceil(max(mag))+1, 1)
+#Bins=[[]]*(len(A)-1)
+#print(Bins)
+for j in range(len(A)-1):
+	idx=[]
+	magn=[]
+	err=[]
+	sd=[]
+	for (i,m,e,s) in zip(idn,mag,error,SD):
+		if (m>A[j] and m<=A[j+1]):
+			idx.append(i)
+			magn.append(m)
+			err.append(e)
+			sd.append(s)
+	data=column_stack((idx, magn, err, sd))
+	savetxt('Bins{0}.txt'.format(j+1), data, fmt='%s')
