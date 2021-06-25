3361d434a03a743f83f6dcabbc09521ad2e93acb
Soumyadeep142 <55705076+Soumyadeep142@users.noreply.github.com>
2021-06-26 02:01:17 +0530

    Find Comparison Stars and Target Stars

--- Comparison.py
+++ Comparison.py
@@ -1,23 +1,72 @@
 from numpy import *
-def f(n):
-	Comparision_star_1_list=['C1 star']
-	Comparision_star_2_list=['C2 star']
-	Magnitude_diff=['Difference']
-	for i in range(1,n+1):
-		idn, mag=loadtxt('Bins{0}.txt'.format(i), unpack='true')
-		mag_diff=100
-		for i in range(len(idn)):
-			for j in range (len(idn)):
-				if j>i:
-					mag_diff_1=abs(mag[i]-mag[j])
-					if (mag_diff_1<mag_diff):
-						mag_diff=mag_diff_1
-						comparision_star=[idn[i], idn[j]]
-					#print(i,j)
-		Comparision_star_1_list.append(comparision_star[0])
-		Comparision_star_2_list.append(comparision_star[1])
-		Magnitude_diff.append(mag_diff)
-		
-	data=column_stack((Comparision_star_1_list, Comparision_star_2_list, Magnitude_diff))
-	savetxt('Comparision_stars.txt', data, fmt = '%s')
-f(5)
+import pandas as pd
+import math
+import statistics
+
+no=2 # Bin number
+
+#For clearing the nan elements
+def clean_list(A):
+	return [x for x in A if str(x) != 'nan']
+	
+id, x_ref, y_ref =loadtxt('refstars_new.list', usecols = (0, 1, 2), unpack = True)
+	
+df=pd.read_csv('stars_with_files.csv')
+df.reset_index(drop=True, inplace=True)
+df.set_index('ID', inplace=True)
+idn,mag=loadtxt('Bins{0}.txt'.format(no), usecols=(0,1), unpack='True')
+
+SD_mag_diff=[]
+index=[]
+for i in range(len(idn)):
+	A=(df.loc[idn[i]].tolist())
+	A=clean_list(A)
+	for j in range(i+1, len(idn)):
+		B=(df.loc[idn[j]].tolist())
+		B=clean_list(B)
+
+		common=intersect1d(A,B)
+		pos1=where(id==idn[i])
+		pos2=where(id==idn[j])
+		x1 = x_ref[pos1].item()
+		y1 = y_ref[pos1].item()
+		x2 = x_ref[pos2].item()
+		y2 = y_ref[pos2].item()
+		print(i,j)
+		mag1=[]
+		mag2=[]
+		a=0
+		index.append([idn[i],idn[j]])
+		for std in common:
+			a+=1
+			#print(i,j,a,len(common))
+			x_ax,y_ax,magn=loadtxt(std, usecols=(1,2,3), skiprows=3, unpack="True")
+			for n in range(len(x_ax)):
+				dist = math.dist([x_ax[n], y_ax[n]], [x1, y1])
+				if dist <= 2:
+					mag1.append(magn[n])
+					break
+			for n in range(len(x_ax)):
+				dist = math.dist([x_ax[n], y_ax[n]], [x2, y2])
+				if dist <= 2:
+					mag2.append(magn[n])
+					break
+		#print(len(mag1), len(mag2))
+		mag_diff=[]
+		for k in range(len(mag1)):
+			mag_diff.append(mag1[k]-mag2[k])
+		SD_mag_diff.append(statistics.stdev(mag_diff))
+
+SD_mag_diff, index = zip(*sorted(zip(SD_mag_diff, index)))
+#print(SD_mag_diff, index)
+C1=index[0][0]
+C2=index[0][1]
+Comparison=[C1, C2]
+data =column_stack((Comparison))
+savetxt('Comparison{0}.txt'.format(no), data, fmt='%s')
+idn=idn.tolist()
+idn.remove(C1)
+#print(idn)
+idn.remove(C2)
+data =column_stack((idn))
+savetxt('Target{0}.txt'.format(no), data, fmt='%s')
