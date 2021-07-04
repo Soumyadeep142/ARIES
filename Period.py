100009ff6a966532a11707c5808dc4a5a33c1e4d
Soumyadeep142 <55705076+Soumyadeep142@users.noreply.github.com>
2021-07-04 13:52:46 +0530

    Add files via upload

--- Period.py
+++ Period.py
@@ -1,5 +1,6 @@
 from numpy import *
 import pandas as pd
+import statistics
 
 for no in range(2,8): #Bin number
 	def clean_list(A):
@@ -16,20 +17,36 @@ for no in range(2,8): #Bin number
 	df_mag.set_index('ID', inplace=True)
 
 	Var_idn=loadtxt('Variable{0}.txt'.format(no))
-
+	Com_idn=loadtxt("Comparison{0}.txt".format(no))
+	C1=Com_idn[0]
+	C=(df.loc[C1].tolist())
+	C=clean_list(C)
 	for i in range(len(Var_idn)):
 		print(i+1, len(Var_idn), 'Bin no {0}'.format(no))
 		T=(df.loc[Var_idn[i]].tolist())
 		T=clean_list(T)
-		S=int(Var_idn[i])
+		
+		S1=int(Var_idn[i])
+		S2=int(C1)
+		common=intersect1d(T,C)
 		Time=[]
-		mag_array=[]
-		for std_i in T:
+		mag_diff_array=[]
+		for std_i in common:
 			#Finding the time
 			ps=where(frame==std_i)
 			time=JD[ps].item()
 			Time.append( round(float(time),6))
-			mag=float(df_mag.loc[S][std_i])
-			mag_array.append(mag)
-		data=column_stack((Time, mag_array))
-		savetxt('{0}.txt'.format(S), data, fmt='%s')
+			mag1=float(df_mag.loc[S1][std_i])
+			mag2=float(df_mag.loc[S2][std_i])
+			mag_diff=mag1-mag2
+			mag_diff_array.append(mag_diff)
+		avg=mean(mag_diff_array)
+		SD=statistics.stdev(mag_diff_array)
+		mag_diff_new=[]
+		Time_new=[]
+		for (i,j) in zip(mag_diff_array, Time):
+			if (i>avg-3*SD and i<avg+3*SD):
+				mag_diff_new.append(i)
+				Time_new.append(j)
+		data=column_stack((Time_new, mag_diff_new))
+		savetxt('{0}.txt'.format(S1), data, fmt='%s')
--- Phase.py
+++ Phase.py
@@ -1,8 +1,8 @@
 from numpy import *
 from matplotlib.pyplot import *
 
-Var_id=18
-P=0.12661176
+Var_id=20952
+P=0.12247171
 Julian_Date, Magnitude=loadtxt('{0}.txt'.format(Var_id), unpack='true')
 Julian_Date,Magnitude=zip(*sorted(zip(Julian_Date,Magnitude)))
 
@@ -10,5 +10,5 @@ JD_i=Julian_Date[0]
 Phase=[]
 for i in range(len(Julian_Date)):
 	Phase.append((Julian_Date[i]-JD_i)/P-int((Julian_Date[i]-JD_i)/P))
-scatter(Phase, Magnitude)
+scatter(Phase, Magnitude,marker='+')
 show()
