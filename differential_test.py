2eca0d5ef03e6f666d8b3c3c94a824d6b5e2e2ff
Bhumika Taneja <75129667+bhumika-11@users.noreply.github.com>
2021-07-13 14:39:32 +0530

    Add files via upload

--- differential_test.py
+++ differential_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 time, mag = np.loadtxt(r"C:\Users\dell\Downloads\541-1842_with_outliers.txt", unpack = True, skiprows = 1, usecols = (0, 1), delimiter = ',')
+print(len(mag))
 
 #Removing outliers
 for i in range(3):
@@ -8,49 +9,44 @@ for i in range(3):
     final_mag = []
     final_time = []
     for (m, t) in zip(mag, time):
-        if m >= mean_mag - 2 * SD_mag and m <= mean_mag + 2 * SD_mag:
+        if m >= mean_mag - 3 * SD_mag and m <= mean_mag + 3 * SD_mag:
             final_mag.append(m)
             final_time.append(t)
     mag = final_mag
     time = final_time
 
+print(len(mag))
 #Removing sudden change
-j = 1
+'''
 t1 = time[1]
 t0 = time[0]
 m1 = mag[1]
 m0 = mag[0]
-
 time_new_list = []
 mag_new_list = []
-while j < len(time) - 2:
-    time_new_list.append(t0)
-    mag_new_list.append(m0)
-    if ((t1 - t0) <= 0.00347):
-        if abs(m1 - m0) > 0.3:
-            t0 = t0
-            m0 = m0
-        else:
-            t0 = t1
-            m0 = m1
-    elif ((t1 - t0) <= 1):
-        if abs(m1 - m0) > 0.4:
-            t0 = t0
-            m0 = m0
-        else:
-            t0 = t1
-            m0 = m1
+'''
+j = 0
+while j < len(mag) - 1:
+    l = len(mag)
+    if np.floor(time[j + 1]) - np.floor(time[j]) == 0.0 and np.abs(mag[j + 1] - mag[j]) > 0.3:
+        time.pop(j + 1)
+        mag.pop(j + 1)
 
-    elif abs(m1 - m0) > 0.5:
-        t0 = t0
-        m0 = m0
+    elif np.floor(time[j + 1]) - np.floor(time[j]) == 1.0 and np.abs(mag[j + 1] - mag[j]) > 0.5:
+        time.pop(j + 1)
+        mag.pop(j + 1)
 
+    elif np.floor(time[j + 1]) - np.floor(time[j]) > 1.0 and np.abs(mag[j + 1] - mag[j]) > 0.6:
+        time.pop(j + 1)
+        mag.pop(j + 1)
+
+    l1 = len(mag)
+    if l == l1:
+        j = j + 1
     else:
-        t0 = t1
-        m0 = m1
-    t1 = time[j + 1]
-    m1 = mag[j + 1]
-    j = j + 1
+        j = j
 
-data = np.column_stack((time_new_list, mag_new_list))
+print(len(mag))
+
+data = np.column_stack((time, mag))
 np.savetxt('test_file.txt', data, fmt = '%s')
