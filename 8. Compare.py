5b20cce73efb92ba43cc5875a0f3f96a3e4a33b0
Soumyadeep142 <55705076+Soumyadeep142@users.noreply.github.com>
2021-07-30 18:29:27 +0530

    Finding Comparison & Target Star in each bin

--- 8. Compare.py
+++ 8. Compare.py
@@ -4,16 +4,16 @@ import math
 import statistics
 from matplotlib.pyplot import *
 
-no=3 # Bin number
+no=7 # Bin number
 
 #For clearing the nan elements
 def clean_list(A):
 	return [x for x in A if str(x) != 'nan']
 	
-id, x_ref, y_ref =loadtxt('refstars_new.list', usecols = (0, 1, 2), unpack = True)
+#id, x_ref, y_ref =loadtxt('refstars_new.list', usecols = (0, 1, 2), unpack = True)
 frame, JD=loadtxt("JD_N_R.list", dtype='str', unpack=True)
 	
-df=pd.read_csv('stars_with_files.csv')
+df=pd.read_csv('second_cut_stars_with_files.csv')
 df.reset_index(drop=True, inplace=True)
 df.set_index('ID', inplace=True)
 idn,mag=loadtxt('Bins{0}.txt'.format(no), usecols=(0,1), unpack='True')
@@ -29,10 +29,10 @@ Time_Frame=[]
 for i in range(len(idn)):
 	A=(df.loc[idn[i]].tolist())
 	A=clean_list(A)
+
 	for j in range(i+1, len(idn)):
 		B=(df.loc[idn[j]].tolist())
 		B=clean_list(B)
-
 		common=intersect1d(A,B)
 		index.append([idn[i],idn[j]])
 		index_all.append([idn[i],idn[j]])
@@ -71,62 +71,6 @@ idn.remove(C1)
 idn.remove(C2)
 data =column_stack((idn))
 savetxt('Target{0}.txt'.format(no), data, fmt='%s')
-
-figure(figsize=(10 ,15))
-#figure, axis = subplots(1, 3)
-
-suptitle('Magnitude Range-{0}-{1}'.format(round(min(mag)), round(max(mag))))	
-	
-for i in range(1,len(index_all)):
-	Final_index=[]
-	Final_mag=[]
-	Final_time=[]
-	if C1 in index_all[i]:
-		Final_index.append(index_all[i])
-		Final_mag.append(mag_diff_all[i])
-		Final_time.append(Time_Frame[i])
-		subplot(3,2,1)
-		scatter((Final_time), (Final_mag),s=3,color='red')
-		xlabel('JD Date')
-		ylabel('Diff Magnitude')
-		title('T-C1')
-
-		break
-
-
-for i in range(len(index_all)):
-	Final_index=[]
-	Final_mag=[]
-	Final_time=[]
-	if C1 in index_all[i]:
-		Final_index.append(index_all[i])
-		Final_mag.append(mag_diff_all[i])
-		Final_time.append(Time_Frame[i])
-		subplot(3,2,4)
-		scatter((Final_time), (Final_mag),s=3,color='red')
-		xlabel('JD Date')
-		ylabel('Diff Magnitude')
-		title('C1-C2')
-
-		break
-
-for i in range(1,len(index_all)):
-	Final_index=[]
-	Final_mag=[]
-	Final_time=[]
-	if C2 in index_all[i]:
-		Final_index.append(index_all[i])
-		Final_mag.append(mag_diff_all[i])
-		Final_time.append(Time_Frame[i])
-		subplot(3,2,5)
-		scatter((Final_time), (Final_mag),s=3,color='red')
-		title('T-C2')
-		xlabel('JD Date')
-		ylabel('Diff Magnitude')
-
-		break
-
-savefig('Bin{0}.png'.format(no))
 
 
 
